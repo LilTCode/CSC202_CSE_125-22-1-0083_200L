7a514b11b68d58661db6d4dceb5513f6bffc7d29
LilTCode <139062730+LilTCode@users.noreply.github.com>
2024-05-27 19:09:20 +0100

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,10 @@
+#program for a band name generator!
+print("Welcome to TESTIMONY'S BAND NAME GENERATOR")
+
+city = input("In which city did you grow up in? \n")
+
+pet = input("What's the name of your pet? \n")
+
+Band_name = str(city) + ' ' + str(pet)
+
+print("Your Band name is: " + Band_name)
